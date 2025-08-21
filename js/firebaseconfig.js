4ca29b63f5c120937f166f398030e62871e907ef
Lars1885 <lpeter1885@gmail.com>
2025-08-21 09:14:27 +0000

    Opdater korrekt firebaseConfig

--- js/firebaseconfig.js
+++ js/firebaseconfig.js
@@ -1,31 +1,15 @@
 // js/firebaseconfig.js
-(function () {
-  // DINE nøgler fra Firebase Console (Project settings → Web app → Config)
-  const firebaseConfig = {
-    apiKey: "AIzaSyCXbznhzgWqKEoEtP6e56h-iEtX32LsxQU",
-    authDomain: "find-my-buddy-2f4d7.firebaseapp.com",
-    projectId: "find-my-buddy-2f4d7",
-    // VIGTIGT: Storage skal ende på .appspot.com
-    storageBucket: "find-my-buddy-2f4d7.appspot.com",
-    messagingSenderId: "463268034142",
-    appId: "1:463268034142:web:b53c112c7fd46dc6766cf",
-    measurementId: "G-2LZB7EVTQV"
-  };
 
-  // Init compat SDK
-  if (!firebase.apps || !firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
+// Din Firebase konfiguration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXbnhzgWxqEoIPE56h-iEtx32klsxQU",
+  authDomain: "find-my-buddy-2f4d7.firebaseapp.com",
+  projectId: "find-my-buddy-2f4d7",
+  storageBucket: "find-my-buddy-2f4d7.appspot.com",
+  messagingSenderId: "463260834142",
+  appId: "1:463260834142:web:b53c112cf764d6cf6766cf",
+  measurementId: "G-2LZB7VETQV"
+};
 
-  // Gør services tilgængelige globalt
-  window.fmb = {
-    auth: firebase.auth(),
-    db: firebase.firestore(),
-    storage: firebase.storage()
-  };
-
-  // Log ind anonymt én gang ved start (safe no-op hvis allerede logget ind)
-  window.fmb.auth.signInAnonymously()
-    .then(() => console.log("[Auth] Anonym login OK"))
-    .catch(err => alert("Kunne ikke logge ind: " + err));
-})();
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
