642b976365767f078d5865f63cb8c69f28a4b49a
Lars1885 <lpeter1885@gmail.com>
2025-09-02 10:21:25 +0000

    config

--- js/firebaseconfig.js
+++ js/firebaseconfig.js
@@ -1,14 +1,12 @@
-
 // js/firebaseconfig.js
 
-// Firebase konfiguration
 const firebaseConfig = {
   apiKey: "AIzaSyCfJSJDYf4_sZwZqNuaIBLa_qHzV4pPFMY",
   authDomain: "find-my-buddy-2.firebaseapp.com",
   projectId: "find-my-buddy-2",
-  storageBucket: "find-my-buddy-2.firebasestorage.app",
+  storageBucket: "find-my-buddy-2.appspot.com",
   messagingSenderId: "913962807255",
-  appId: "1:913962807255:web:81d0912386f97b12e2592",
+  appId: "1:913962807255:web:81d09312386f97b12e2592",
   measurementId: "G-4RS0KM5CWS"
 };
 
